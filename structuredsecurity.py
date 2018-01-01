b482d6d9a504defde7de5cbc8fe6b94e35ac1ae1
gjimzhou <gjimzhou@gmail.com>
2018-01-01 20:31:20 +0800

    StructuredSecurity.makePayments.()

--- structuredsecurity.py
+++ structuredsecurity.py
@@ -9,16 +9,17 @@ from tranche import *
 
 
 class StructuredSecurity(object):
-    def __init__(self, totalNotional):
+    def __init__(self, totalNotional, mode):
         self._totalNotional = totalNotional
+        self._mode = mode
         self._tranches = []
 
     @property
     def totalNotional(self):
         return self._totalNotional
 
-    def addTranche(self, notional, rate, subordination):
-        self._tranches.append(StandardTranche(notional, rate, subordination))
+    def addTranche(self, percent, rate, subordination):
+        self._tranches.append(StandardTranche(self._totalNotional * percent, rate, subordination))
         self._tranches.sort(key=lambda t: t.subordination(), reverse=True)
 
     def increaseTimePeriods(self):
@@ -31,3 +32,15 @@ class StructuredSecurity(object):
             interestPayment = min(cashAmount, interestDue)
             t.makeInterestPayment(interestPayment)
             cashAmount -= interestPayment
+
+        if self._mode == 'Sequential':
+            for t in self._tranches:
+                notionalBalance = t.notionalBalance()
+                principalPayment = min(cashAmount, notionalBalance)
+                t.makePrincipalPayment(principalPayment)
+                cashAmount -= principalPayment
+        elif self._mode == 'Pro Rata':
+            for t in self._tranches:
+                percent = t.notional() / self._totalNotional
+                principalPayment = cashAmount * percent
+                t.makePrincipalPayment(principalPayment)
